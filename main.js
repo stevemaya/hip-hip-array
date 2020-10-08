8fb04392136b519005b285b5c7dd7cf7783060f7
Steve Maya <steve.maya@codeimmersives.com>
2020-10-07 21:10:33 -0500

    up to challenge 10 and stuck now

--- main.js
+++ main.js
@@ -19,43 +19,105 @@ const students = [
   'Steve',
 ];
 
+//for (let i = 0; i < students.length; i++)
+//console.log(students[i]);
 
 // Challenge 2
 // Loop through the following array BACKWARDS, console.logging out each value. 64 should be printed first and 100 last.
 const grades = [100, 80, 110, 75, 83, 64];
 
+/*
+for (let i = grades.length - 1; i >= 0; i--) {
+  console.log(grades[i])
+}
+*/
 
 // Challenge 3
 // Console.log out only the even numbers in the following array.
-const positiveNumbers = [5, 2, 13, 17, 4, 102, 3000];
 
+/*
+const positiveNumbers = [5, 2, 13, 17, 4, 102, 3000];
+for (let i = 0; i < positiveNumbers.length; i++) {
+  if (positiveNumbers[i] % 2 === 0) {
+  console.log(positiveNumbers[i]); 
+  }
+}
+*/
 
 // Challenge 4
 // Console.log out the even numbers in the following array, INCLUDING the negative ones. There should be four total printings.
 const mixedSignNumbers = [3, 15, 14, -2, -3, -8, -103, 4];
 
+/*
+for (let i = 0; i < mixedSignNumbers.length; i++) {
+  if (mixedSignNumbers[i] % 2 === 0) {
+  console.log(mixedSignNumbers[i]); 
+  }
+}
+*/
 
 // Challenge 5
 // Remove two values from the beginning and one value from the end of the following array, simply by deleting them in the following line. Then console.log out each value individually.
 const symmetricalCapitals = ['A', 'H', 'I', 'M', 'O', 'T', 'U', 'V', 'W', 'X', 'Y'];
 
+/*
+symmetricalCapitals.shift();
+symmetricalCapitals.shift();
+symmetricalCapitals.pop();
+
+for (let i = 0; i < symmetricalCapitals.length; i++) {
+console.log(symmetricalCapitals[i]);
+}
+*/
 
 // Challenge 6
 // Add a value (your choice!) to the start of the following array and two values (of your choice) to the end simply by adding them in the following line, and then console.log out each value individually.
 const fibonacciNumbers = [1, 1, 2, 3, 5, 8, 13];
 
+/*
+fibonacciNumbers.unshift(9999);
+fibonacciNumbers.push(1011, 1035);
+
+for (let i = 0; i < fibonacciNumbers.length; i++) {
+  console.log(fibonacciNumbers[i]);
+}
+*/
 
 // Challenge 7
 // Make a NEW array with 5 values of your choice, using the same syntax as the array creation from the previous 6 challenges; that is, every value should be placed within the square brackets.  Now console.log out each value individually.
 
+const newArray = ["Let's", 'be', 'friends', 'or', 'else.']
+
+/*
+for (let i = 0; i < newArray.length; i++) {
+  console.log(newArray[i]);
+}
+*/
 
 // Challenge 8
 // Make a new EMPTY array and fill it with five values of your choice using BOTH .push AND .unshift, and then console.log out each value individually.
 
+const newEmptyArray = [];
+
+/*
+newEmptyArray.unshift('Hello', 'my', 'name');
+newEmptyArray.push('is', 'Steve', 'Maya');
+
+for (let i = 0; i <newEmptyArray.length; i++) {
+  console.log(newEmptyArray[i]);
+}
+*/
 
 // Challenge 9
 // Loop through the `students` array from Challenge 1, starting at index 3 and ending at index 10 (INCLUSIVE), logging out each value individually.
 
+const newStudents = students.splice(3, 10);
+
+/*
+for(let i = 0; i < newStudents.length; i++) {
+  console.log(newStudents[i]);
+}
+*/
 
 // Challenge 10
 // Loop through the `students` array from Challenge 1, making a COPY of the array, starting at index 3 and ending at index 10 (INCLUSIVE). This is very similar to string building; create a new array, loop through the old one, and put values from the original in the new one. Then log out each value of the copy indivudally.
